dd52acc86c32aa58652d467a1a2bb24b6017c259
lalettywambo <lalettywambo@gmail.com>
2026-06-11 00:54:33 +0300

    add code content to js file

---
+++ index.js
@@ -0,0 +1,25 @@
+// TERNARY OPERATOR= a shortcut to if{} and else{} statements
+//                helps to assign a variable based on a condition
+//                condition? codeIfTrue : codeIfFalse;
+        //EXAMPLE 1:
+/*let age = 10;
+let message = age >= 18 ? "You're an adult" : "You're a minor";
+console.log(message);
+*/
+
+         //EXAMPLE 2:
+/*let time = 10 ;
+let greeting = time < 12? "Good Morning" : "Good Afternoon"
+console.log(greeting);
+*/
+
+        //EXAMPLE 3:
+/*let isStudent = false;
+let message = isStudent ? "You are a student" : "You are not a student"
+console.log(message);
+*/
+
+      //EXAMPLE 4:
+let purchaseAmount = 55
+let message = purchaseAmount >=100? "You get a 10% discount" : "You pay normal price"
+console.log(message);
